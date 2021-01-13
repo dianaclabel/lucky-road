7f2eafa55c4bbea6233ac4e9bd717488351c832c
diana clabel <dianaclabel08@gmail.com>
2021-01-12 20:52:22 -0500

    avance del metodo init

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-console.log("Hola, mundo.")
+const game = {
+    players: [],
+    squares: [],
+    turn: -1,
+    init: () => {}
+}
+
+game.init = (playersLength,squaresLength) => {
+    // generate players
+    
+    for (let i = 0; i < playersLength; i++){
+        // generate a player
+        const player = {
+            position: -1,
+        }
+        game.players.push(player)
+    }
+
+    // generate squares 
+    for(let i = 0; i < squaresLength; i++){
+        const square = {
+            luck: null
+        }
+        game.squares.push(square)
+    }
+    
+    // set the turn of the first player
+    game.turn = getRandomInt(playersLength) 
+ 
+}
+
+game.init(3,50);
+console.log(game);
+
+
+function getRandomInt(max) {
+    const random = Math.random()
+    const decimalResult = random * max
+    return Math.floor(decimalResult)
+}
